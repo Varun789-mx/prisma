c720436c7dfa52e8e55abc404a303b83600ee579
varun789-mx <varunsh153@gmail.com>
2025-03-15 19:57:08 +0000

    add file create_user.ts

---
+++ src/create_user.ts
@@ -0,0 +1,19 @@
+import {PrismaClient} from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+
+async function main() { 
+
+}
+
+main() 
+.then(async () => {
+	await prisma.$disconnect()
+	console.log("query run successfull")
+})
+	.catch(async (e) => { 
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
