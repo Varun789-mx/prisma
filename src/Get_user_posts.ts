1666d79a06e5d7734ea8a38dc19a179e8f8eeb61
varun789-mx <varunsh153@gmail.com>
2025-03-18 01:07:06 +0530

    Fixed errors in Get_user_posts

--- src/Get_user_posts.ts
+++ src/Get_user_posts.ts
@@ -1,28 +1,32 @@
 import { PrismaClient } from '@prisma/client'
 
-const prisma = new PrismaClient({log:['info','query'],})
+const prisma = new PrismaClient({ log: ['info', 'query'], })
 
-interface User { 
-	id:number;
-	email:string;
-	name:string;
+interface User {
+	id: number;
 }
 
-async function Getpost(prop:User) {
-       try{	
+async function Getpost(prop: User) {
+
 	const post = await prisma.post.findUnique({
 		where: {
-			id:prop.id
+			id: prop.id
 		},
 	})
 
 	console.log(post)
 	prisma.$disconnect()
-       }
- .catch(async(e)=> { 
-	 await prisma.$disconnect()
-	 console.log(e)
- })
+
 }
+Getpost({ id: 1 })
+	.then(async () => {
+		prisma.$disconnect()
+		console.log("Query ran successfully")
+	})
+	.catch(async (e) => {
+		await prisma.$disconnect()
+		console.log(e)
+	})
+
 
 export default Getpost
