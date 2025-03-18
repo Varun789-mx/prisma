30be0ab768721a1da4661411915cc29fe2878d01
Varun789-mx <varunsh153@gmail.com>
2025-03-18 07:22:59 +0000

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+
+const port = 5000;
+
+const app = express();
+
+app.get('/',(req,res)=>{ 
+	res.send("Hello world");
+});
+
+app.listen(port,()=>{ 
+	console.log(`Server started on ${port}`);
+})
+;
+
+
