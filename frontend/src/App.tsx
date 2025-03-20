1be5683457d68ae9021ed1e3715596ed62821c59
varun789-mx <varunsh153@gmail.com>
2025-03-21 00:22:41 +0530

    Add tailwind and fix the App.tsx design

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,21 +2,22 @@ import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  interface prop { 
-    name:string;
-    email:string;
+  interface prop {
+    id: number;
+    name: string;
+    email: string;
   }
   const [user, setuser] = useState<prop[]>([]);
   const url = 'http://localhost:5000/getuser';
   const fetchuser = useCallback(async () => {
     try {
-    const response = await fetch(url, {
-      method: 'GET',
-      mode: 'cors', // Explicitly specify CORS mode
-      headers: {
-        'Accept': 'application/json'
-      }
-    });
+      const response = await fetch(url, {
+        method: 'GET',
+        mode: 'cors', // Explicitly specify CORS mode
+        headers: {
+          'Accept': 'application/json'
+        }
+      });
       if (!response.ok) {
         console.log(`Response code:${response.status}`);
         return;
@@ -33,16 +34,20 @@ function App() {
   }, [fetchuser]);
   return (
     <>
-      <div>
-        {user.length > 0 ? (user.map((data:prop, index:number) => (
+      <div className='bg-gray-800 text-white'>
+        {user.length > 0 ? (user.map((data: prop, index: number) => (
           <div key={index}>
-            <h1>{data.name}</h1>
-            <p>{data.email}</p>
+            <div className='block flex' >
+
+              <div className='w-1/5 p-1 m-0.5 border-1 border-gray-400'><p className='font-sans'>ID: {data.id}</p></div>
+              <div className='w-1/5 p-1 m-0.5 border-1 border-gray-400'><p className='font-sans'>Name: {data.name}</p></div>
+              <div className='w-1/5 p-1  m-0.5 border-1 border-gray-400'><p className='font-sans'>Email: {data.email}</p></div>
+            </div>
           </div>
-        ))): (
+        ))) : (
           <p>Loading....</p>
         )}
-        
+
       </div>
     </>
   )
