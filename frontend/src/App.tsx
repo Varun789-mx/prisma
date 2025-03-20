3cafce4a627aa8fa060e250d8a12ff9c1de92e91
varun789-mx <varunsh153@gmail.com>
2025-03-20 22:59:20 +0530

    Fix the merge conflict in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+
 import { useCallback, useEffect, useState } from 'react'
 import './App.css'
 
@@ -44,8 +45,8 @@ function App() {
         )}
         
       </div>
-    </>
-  )
+      </>
+)
 }
 
-export default App
+export default App;
